30f9069874617b9a36db58e3781d11a47ce3cfca
jitinfullstack <jitinfullstack@gmail.com>
2022-06-27 18:49:01 +0530

    all setup done.

--- db/connect.js
+++ db/connect.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 
 const connectDB = (url) => {
     return mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true }).then(() => console.log('CONNECTED TO THE DB...')).catch((err) => console.log(err));
-    // mongoose.connect(connectionString, { useNewUrlParser: true }).then(() => { console.log('mongoDB is connected...') }).catch((err) => { console.log(err) });
 }
 
 module.exports = connectDB
